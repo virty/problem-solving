829097e1ccbd2d68727af46a24c54d577bccf761
virty <virty@naver.com>
2017-06-14 00:11:33 +0900

    Hash

--- ProblemSolving/BFS_algorithm/Source.cpp
+++ ProblemSolving/BFS_algorithm/Source.cpp
@@ -1,102 +1,70 @@
 #include <stdio.h>
 
-#define MAX_N 50
+int T, head, rear, R, C;
+int queue[50 * 50][3];
+int map[52][52];
 
-int MAP[MAX_N + 2][MAX_N + 2];
-int queue[MAX_N * MAX_N][3];
-int row;
-int column;
-int head;
-int rear;
-
-int isEmpty()
-{
-	return (head <= rear) ? 1 : 0;
-}
-
-int enqueue(int x, int y, int c)
+void enqueue(int x, int y, int d)
 {
 	queue[head][0] = x;
 	queue[head][1] = y;
-	queue[head][2] = c;
+	queue[head][2] = d;
 	head++;
-	return 1;
 }
 
-int dequeue(int *x, int *y, int *c)
+void dequeue(int *x, int *y, int *d)
 {
-	if (isEmpty())
-	{
-		return 0;
-	}
 	*x = queue[rear][0];
 	*y = queue[rear][1];
-	*c = queue[rear][2];
+	*d = queue[rear][2];
 	rear++;
-	return 1;
 }
 
-int breadthFirstSearch()
+int bfs()
 {
-	int x;
-	int y;
-	int c;
-
+	int x, y, d;
 	enqueue(1, 1, 0);
-	MAP[1][1] = 0;
-	while (!isEmpty())
+	map[1][1] = 0;
+	while (rear <= head)
 	{
-		dequeue(&x, &y, &c);
-		if (x == column && y == row)
+		dequeue(&x, &y, &d);
+		if (x == R && y == C)
+			return d;
+		if (x + 1 <= R && map[x + 1][y])
 		{
-			return c;
+			enqueue(x + 1, y, d + 1);
+			map[x + 1][y] = 0;
 		}
-		if (x + 1 <= column && MAP[x + 1][y])
+		if (x - 1 > 0 && map[x - 1][y])
 		{
-			enqueue(x + 1, y, c + 1);
-			MAP[x + 1][y] = 0;
+			enqueue(x - 1, y, d + 1);
+			map[x - 1][y] = 0;
 		}
-		if (y + 1 <= row && MAP[x][y + 1])
+		if (y + 1 <= C && map[x][y + 1])
 		{
-			enqueue(x, y + 1, c + 1);
-			MAP[x][y + 1] = 0;
+			enqueue(x, y + 1, d + 1);
+			map[x][y + 1] = 0;
 		}
-		if (x - 1 > 0 && MAP[x - 1][y])
+		if (y - 1 > 0 && map[x][y - 1])
 		{
-			enqueue(x - 1, y, c + 1);
-			MAP[x - 1][y] = 0;
-		}
-		if (y - 1 > 0 && MAP[x][y - 1])
-		{
-			enqueue(x, y - 1, c + 1);
-			MAP[x][y - 1] = 0;
+			enqueue(x, y - 1, d + 1);
+			map[x][y - 1] = 0;
 		}
 	}
 	return -1;
 }
 
-
-int main(void)
+int main()
 {
-	int test_case;
-	int T;
-
 	scanf("%d", &T);
-
-	for (test_case = 1; test_case <= T; test_case++)
+	for (int tc = 1; tc <= T; tc++)
 	{
-		head = 0;
-		rear = 0;
-		scanf("%d %d", &row, &column);
-
-		for (int i = 1; i <= row; i++)
-		{
-			for (int j = 1; j <= column; j++)
-			{
-				scanf("%d", &MAP[j][i]);
-			}
-		}
-		printf("#%d %d\n", test_case, breadthFirstSearch());
+		head = rear = 0;
+		scanf("%d %d", &R, &C);
+		for (int i = 1; i <= R; i++)
+			for (int j = 1; j <= C; j++)
+				scanf("%d", &map[i][j]);
+		printf("#%d %d\n", tc, bfs());
 	}
 	return 0;
 }
